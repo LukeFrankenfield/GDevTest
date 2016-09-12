4db50db475ddfbf2681d0d77ecfec06c26c0b0b1
Luke Frankenfield <lf0435@ship.edu>
2016-09-12 08:12:14 -0400

    Add files via upload

---
+++ core/src/com/packtpub/libgdx/canyonbunny/CanyonBunnyMain.java
@@ -0,0 +1,62 @@
+package com.packtpub.libgdx.canyonbunny;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.packtpub.libgdx.canyonbunny.game.*;
+
+public class CanyonBunnyMain implements ApplicationListener {
+	
+	private static final String TAG = CanyonBunnyMain.class.getName();
+	private WorldController worldController;
+	private WorldRenderer worldRenderer;
+	private boolean paused;
+	
+	@Override
+	public void create () {
+		// Set Libgdx log level to DEBUG
+		Gdx.app.setLogLevel (Application.LOG_DEBUG);
+		//Initialize controller and renderer
+		worldController = new WorldController();
+		worldRenderer = new WorldRenderer(worldController);
+		//Game world is active on start
+		paused = false;
+	}
+
+	@Override
+	public void render () {
+		if (!paused){
+		//Update game world by the time that has passed since last rendered frame
+		worldController.update(Gdx.graphics.getDeltaTime());
+		}
+		//Sets the clear screen color to: Cornflower blue
+		Gdx.gl.glClearColor(0x64/255.0f, 0x95/225.0f, 0xed/255.0f, 0xff/255.0f);
+		
+		//Clears screen
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		//Render Game world
+		worldRenderer.render();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		worldRenderer.resize(width, height);
+	}
+
+	@Override
+	public void pause() {
+		paused = true;
+	}
+
+	@Override
+	public void resume() {
+		paused = false;
+	}
+
+	@Override
+	public void dispose() {
+		worldRenderer.dispose();
+	}
+}
---
+++ core/src/com/packtpub/libgdx/canyonbunny/util/Constants.java
@@ -0,0 +1,7 @@
+package com.packtpub.libgdx.canyonbunny.util;
+
+public class Constants {
+	//Visible Game world is 5m wide and 5m Tall
+	public static final float VIEWPORT_WIDTH = 5.0f;
+	public static final float VIEWPORT_HEIGHT = 5.0f;
+}
